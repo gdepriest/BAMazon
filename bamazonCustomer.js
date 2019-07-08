fd22f091e9c5e62f6df8ce9a930bc51914f242ef
Genevieve DePriest <gdpparton@gmail.com>
2019-07-07 22:54:14 -0600

    I added the else for when the user doesn't want to proceed with purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -137,6 +137,8 @@ function buyBamazon() {
                             
                         });
         
+                    } else {
+                        buyBamazon();
                     };
 
 
